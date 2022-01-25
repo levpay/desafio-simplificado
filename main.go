dffcc9013eb117d9f81cfefcbc70e6545f5dc772
Pedro <pedro_fejes@hotmail.com>
2022-01-24 22:58:11 -0300

    feature: Testando função Jogo

--- main.go
+++ main.go
@@ -5,25 +5,37 @@ import ( //Biblioteca
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	fmt.Println("Escolha entre Cara ou Coroa:") //Mostra as alternativas
-	reader := bufio.NewReader(os.Stdin)         //Pede ao usuário a escolha
-	input, err := reader.ReadString('\n')       // Lê a entrada do usuário
+	fmt.Println("Cara: 0")
+	fmt.Println("Coroa: 1")
+	reader := bufio.NewReader(os.Stdin)   //Pede ao usuário a escolha
+	input, err := reader.ReadString('\n') // Lê a entrada do usuário
 	if err != nil {
 		fmt.Println(err) // Se der erro, vai imprimi-lo
 	}
-	fmt.Println(input) // Se não der erro imprimi
+	input = strings.TrimSuffix(input, "\n") //Remove o \n do final da string
+	opcao, err := strconv.Atoi(input)       // converte a string para inteiro
 
+	if opcao > 1 { //Se o usuário digitar uma opção inválida
+		fmt.Println("Opção Inválida") //Imprimi a mensagem
+		return
+	}
+	status, result := jogo(opcao) // Chama a função jogo
+	fmt.Println(status)
+	fmt.Println(result)
 }
 func jogo(opcao int) (int, string) { //Retorna o Status do jogo e o resultado
 	rand.Seed(time.Now().UnixNano())                  //Gera um número aleatório
 	options := []string{"cara", "coroa"}              // Opções possíveis
 	result := rand.Intn(2)                            //Gera um número aleatório entre 0 e 1
-	fmt.Println("O resultado foi: ", result)          //Imprime o resultado
-	fmt.Println("O resultado foi: ", options[result]) //Imprime o resultado
+	fmt.Println("O resultado foi: ", result)          //Imprime o resultado (Debug)
+	fmt.Println("O resultado foi: ", options[result]) //Imprime o resultado (Debug)
 
 	if options[result] == options[opcao] {
 		return 1, options[result] //Retorna 1 se o usuário ganhou
